5a9498a5684ffc2fca795b2947be16bcdf4cb13e
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2022-10-31 00:37:54 +0100

    API

---
+++ backend-stage_1_task/index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.get('/emmanuel-julius', (req, res) => {
+        res.status(200).send({
+                slackUsername: "Emmanuel Julius",
+                backend: true,
+                age: 20,
+                bio: "An aspiring full-stack developer with a curious personality"
+        })
+});
+
+app.listen(PORT)
